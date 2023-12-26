136f4f00bff361e4b8b9bdbefde1ee1ba9572dba
Milan Petrovic <5rovicmilan8@gmail.com>
2023-12-26 23:55:24 +0100

    backup

---
+++ 1.c
@@ -0,0 +1,21 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define check_error(cond, msg)\
+    do{\
+        if(!(cond)){\
+            perror(msg);\
+            exit(1);\
+        }\
+    } while(0)
+
+int main(int argc, char **argv){
+    
+}
